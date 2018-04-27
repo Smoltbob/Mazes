4831ee4bfad730f4845f0613ee708e5f161d861e
Jules Simon <jules.simon@ensimag.fr>
2018-04-27 15:54:17 +0200

    Added cube

---
+++ libs/cube_grid.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from grid import *
+
+class Cube_cell(Cell):
+    
+    def __init__(self, face, row, column):
+        self.face = face
+        super(row, column)
+
+class Cube_grid(Grid):
+
+    def __init__(self, dim):
+        super(dim, dim)
+    
+    def prepare_grid(self):
+
--- libs/masked_grid.py
+++ libs/masked_grid.py
@@ -3,14 +3,6 @@ from grid import *
 
 class Masked_grid(Grid):
 
-    def __init(self, rows, columns):
-       Grid.__init__(self, rows, columns)  
-    
-    def __getitem__(self, key):
-        row, col = key
-        if row < 0 or row > self.rows - 1:
-            return None
-        else:
-            col = col % self.columns
-            return self.grid[row][col]
-
+    def __init(self, mask):
+        self.mask = mask
+        
